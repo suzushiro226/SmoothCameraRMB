f8a83fb01d3ee5729a1a1557b5d01cb7870a4fa8
suzushiro226 <suzushiro226@gmail.com>
2021-05-20 23:51:55 +0900

    Revert "Initial commit"
    This reverts commit 964d3c61da648465346ca8ccb8d1559d598a5336.

---
+++ SmoothCameraRMB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace SmoothCameraRMB
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Main : MonoBehaviour
+    {
+        bool d;
+        Vector3 p, v;
+        readonly float s = FlightCamera.fetch.orbitSensitivity;
+
+        void Update()
+        {
+            if (CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.Flight)
+            {
+                var c = FlightCamera.fetch;
+
+                if (Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject())
+                {
+                    p = Input.mousePosition;
+                    d = true;
+                    c.orbitSensitivity = 0;
+                }
+
+                if (!Input.GetMouseButton(1))
+                {
+                    d = false;
+                    c.orbitSensitivity = s;
+                }
+
+                var q = Input.mousePosition;
+
+                if (d)
+                {
+                    if (Input.GetKey(KeyCode.LeftAlt))
+                        v.z += (q.y - p.y) / 10000f;
+                    else
+                        v += (q - p) / 10000f;
+                }
+
+                c.camHdg += v.x;
+                c.camPitch -= v.y;
+                c.SetDistance(c.Distance * (1 - v.z));
+
+                p = q;
+                v *= Input.GetMouseButton(0) ? 0.9f : (Input.GetMouseButton(1) ? 1 : 0.99f);
+            }
+        }
+    }
+}
